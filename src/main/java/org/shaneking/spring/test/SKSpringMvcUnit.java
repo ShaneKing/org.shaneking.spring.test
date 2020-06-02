ed0bc49da925140bd38506ad9cd27c8ee2933777
ShaneKing <github@azolla.org>
2020-06-03 00:57:50 +0800

    0.3.3

--- src/main/java/org/shaneking/spring/test/SKSpringMvcUnit.java
+++ src/main/java/org/shaneking/spring/test/SKSpringMvcUnit.java
@@ -2,9 +2,14 @@ package org.shaneking.spring.test;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import lombok.Getter;
+import lombok.Setter;
 import org.shaneking.skava.io.FTN;
 import org.shaneking.skava.lang.String0;
+import org.shaneking.skava.util.Map0;
+import org.shaneking.skava.util.Randomizer;
 import org.shaneking.skava.util.UUID0;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +23,7 @@ import javax.servlet.http.Cookie;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -32,6 +38,18 @@ public class SKSpringMvcUnit extends SKSpringUnit {
   @Autowired
   @Getter
   public MockMvc mockMvc;
+  @Getter
+  @Setter
+  private Map<String, Randomizer<String>> tstReplaceMap = Map0.newHashMap(Lists.newArrayList("$UUID1$"), Lists.newArrayList((Randomizer<String>) UUID0::cMl33));
+  @Getter
+  @Setter
+  private String tstUploadFileFieldName = "file";
+  @Getter
+  @Setter
+  private String tstUploadParamFieldName = "req";
+  @Getter
+  @Setter
+  private String tstUploadContentType = "text/plain";
 
   public MockHttpServletRequestBuilder cookie(MockHttpServletRequestBuilder mockHttpServletRequestBuilder) {
     return mockHttpServletRequestBuilder.cookie(new Cookie("skToken", "skTestToken"));
@@ -47,20 +65,33 @@ public class SKSpringMvcUnit extends SKSpringUnit {
 
   public void perform(String url, File reqJsonFile, File reqAttachFile, File respJsonFile, File respTxtFile) throws Exception {
     String uuid1 = UUID0.cMl33();
-    String reqStr = Joiner.on(String0.EMPTY).join(Files.readAllLines(reqJsonFile.toPath(), StandardCharsets.UTF_8)).replace("$UUID1$", uuid1);
+    Map<String, String> replaceMap = Maps.newHashMap();
+    this.getTstReplaceMap().keySet().parallelStream().forEach(k -> replaceMap.put(k, this.getTstReplaceMap().get(k).next()));
+    String reqStr = Joiner.on(String0.EMPTY).join(Files.readAllLines(reqJsonFile.toPath(), StandardCharsets.UTF_8));
+    for (String key : replaceMap.keySet()) {
+      reqStr = reqStr.replace(key, replaceMap.get(key));
+    }
     ResultActions resultActions = null;
     if (reqAttachFile == null) {
       resultActions = mockMvc.perform(appJson(cookie(post(url).content(reqStr))));
     } else {
-      resultActions = mockMvc.perform(multiFormData(cookie(multipart(url).file(new MockMultipartFile("file", reqAttachFile.getName(), "text/plain", Files.readAllBytes(reqAttachFile.toPath()))).param("req", reqStr))));
+      resultActions = mockMvc.perform(multiFormData(cookie(multipart(url).file(new MockMultipartFile(this.getTstUploadFileFieldName(), reqAttachFile.getName(), this.getTstUploadContentType(), Files.readAllBytes(reqAttachFile.toPath()))).param(this.getTstUploadParamFieldName(), reqStr))));
     }
     resultActions.andExpect(status().isOk()).andDo(print());
     if (respJsonFile != null) {
-      resultActions.andExpect(content().string(Joiner.on(String0.EMPTY).join(Files.readAllLines(respJsonFile.toPath(), StandardCharsets.UTF_8).stream().map(String::trim).collect(Collectors.toList())).replace(": ", ":").replace("$UUID1$", uuid1)));
+      String respStr = Joiner.on(String0.EMPTY).join(Files.readAllLines(respJsonFile.toPath(), StandardCharsets.UTF_8).stream().map(String::trim).collect(Collectors.toList()));
+      respStr = respStr.replace(": ", ":");
+      for (String key : replaceMap.keySet()) {
+        respStr = respStr.replace(key, replaceMap.get(key));
+      }
+      resultActions.andExpect(content().string(respStr));
     } else if (respTxtFile != null) {
       for (String s : Files.readAllLines(respTxtFile.toPath(), StandardCharsets.UTF_8)) {
         if (!Strings.isNullOrEmpty(s)) {
-          resultActions.andExpect(content().string(containsString(s.replace("$UUID1$", uuid1))));
+          for (String key : replaceMap.keySet()) {
+            s = s.replace(key, replaceMap.get(key));
+          }
+          resultActions.andExpect(content().string(containsString(s)));
         }
       }
     }
--- src/main/java/org/shaneking/spring/test/SKSpringUnit.java
+++ src/main/java/org/shaneking/spring/test/SKSpringUnit.java
@@ -2,8 +2,10 @@ package org.shaneking.spring.test;
 
 import org.junit.runner.RunWith;
 import org.shaneking.test.SKUnit;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class SKSpringUnit extends SKUnit {
 }
